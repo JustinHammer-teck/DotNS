fc0c09d3b4d5cb00a282c678dd523107f0216699
Moritz Zimmermann <moritzzmn@Moritzs-MacBook-Pro.local>
2024-05-31 13:17:24 +1000

    Stuck, figuring out how to work with DNS header

---
+++ src/DotNS/Models/RequestPkg.cs
@@ -0,0 +1,9 @@
+using System.Net;
+
+namespace DotNS.Models
+{
+    public class RequestPkg
+    {
+        public IPAddress IPAddress { get; set; }
+    }
+}
--- src/DotNS/Program.cs
+++ src/DotNS/Program.cs
@@ -2,48 +2,54 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using DotNS;
 
-var buffer = new byte[1_024]; // buffer can be define base on the need of the server and the intentional use case
-
-Helper.TryParseIpAddress("192.168.0.176:9500", out var sumip, out var port);
-
-var ip = new IPAddress(sumip);
-var endpoint = new IPEndPoint(ip, 9500);
-
-using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.PacketInformation, true);
-socket.Bind(endpoint);
-
-var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
-var remote = (EndPoint)remoteEndPoint;
-var receiveData = socket.ReceiveFrom(buffer, ref remote);
-
-socket.Connect(remote);
-
-Console.WriteLine($"ReceiveFrom {remote.ToString()}, with message: {Encoding.UTF8.GetString(buffer, 0, receiveData)}");
-
-var responseData = Encoding.UTF8.GetBytes("Welcome to DotNS");
-socket.SendTo(responseData, 0, responseData.Length, SocketFlags.None, remote);
-
-
-try
+internal class Program
 {
-    while (true)
+    private static void Main(string[] args)
     {
-        buffer = new byte[1_024];
-        receiveData = socket.Receive(buffer);
+        var buffer = new byte[1_232]; // buffer can be define base on the need of the server and the intentional use case
 
-        /*
-         * this is to encode the bytes receive from Client to human-readable string
-         * */
-        var data = Encoding.UTF8.GetString(buffer, 0, receiveData);
+        var endpoint = new IPEndPoint(IPAddress.Parse("10.126.138.17"), 9500);
 
-        Console.WriteLine(data);
+        using var socket = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Dgram,
+            ProtocolType.Udp
+        );
+        socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.PacketInformation, true);
+        socket.Bind(endpoint);
+
+        var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        var remote = (EndPoint)remoteEndPoint;
+        var receiveData = socket.ReceiveFrom(buffer, ref remote);
+
+        Console.WriteLine(
+            $"ReceiveFrom {remote.ToString()}, with message: {Encoding.ASCII.GetString(buffer, 0, receiveData)}"
+        );
+
+        var responseMsg = Encoding.ASCII.GetBytes("10.10.10.126");
+
+        Console.WriteLine(responseMsg.Length);
+
+        var responseData = buffer[..13].Concat(responseMsg).ToArray();
+
+        socket.SendTo(responseData, 0, responseData.Length, SocketFlags.None, remote);
+
+        try
+        {
+            while (true)
+            {
+                buffer = new byte[1_232];
+                receiveData = socket.Receive(buffer);
+
+                var data = Encoding.ASCII.GetString(buffer, 0, receiveData);
+
+                Console.WriteLine(data);
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 }
-catch (Exception)
-{
-    throw;
-}
-
